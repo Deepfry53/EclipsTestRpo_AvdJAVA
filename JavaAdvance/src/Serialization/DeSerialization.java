6bea4b63c351d47446f2ed729842bf16592abde1
Deep_fry <sdeepak8087@gmail.com>
2024-09-13 12:14:32 +0530

    Jdbc Update and Generics

--- JavaAdvance/src/Serialization/DeSerialization.java
+++ JavaAdvance/src/Serialization/DeSerialization.java
@@ -1,7 +1,6 @@
 package Serialization;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
--- JavaAdvance/src/Serialization/Serialization.java
+++ JavaAdvance/src/Serialization/Serialization.java
@@ -1,9 +1,7 @@
 package Serialization;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Serialization {
@@ -13,10 +11,7 @@ public class Serialization {
 		
 		Student std = new Student("deepak", 101, 22);
 		
-//		std.RollNumber = 31;
-//		std.name = "deepak";
-		
-//		System.out.println(" this is the best " + std);
+
 		
 		String fileName = "/Users/deepak/Desktop/programing/Java/AdvanceJava/JavaAdvance/data/SerializationTest.txt";
 		FileOutputStream fileOut= null; 
@@ -38,26 +33,6 @@ public class Serialization {
 			
 		}
 		
-		
-
-//		FileInputStream fileIn = null;
-//		ObjectInputStream objIn = null;
-//		
-//		
-//		try {
-//			fileIn = new FileInputStream(fileName);
-//			objIn = new ObjectInputStream(fileIn);
-//			
-//			Student newstd = (Student)objIn.readObject();
-//			
-//			System.out.println( newstd );
-//			
-//		} catch (IOException ex) {
-//			// TODO: handle exception
-//		} finally {
-//			fileIn.close();
-//			objIn.close();
-//		}
 
 	}
 
